c2ffa2cf702b4f0980150f84815609a8f3ea6a39
DESKTOP-T9T2COA\420 <rtm.tsalisher@gmail.com>
2024-01-05 15:30:13 +0200

    5.1 LP Filter with auxiliary variable

--- Filters With Auxiliary Variable/AuxiliaryCoeffsLP.m
+++ Filters With Auxiliary Variable/AuxiliaryCoeffsLP.m
@@ -7,19 +7,4 @@ den = 1 + sqrt(2)*k + k^2;
 b0 = (k^2)/den; b1 = (2*k^2)/den; b2 = (k^2)/den;
 a0 = 1; a1 = 2*(k^2-1)/den; a2 = (1-sqrt(2)*k + k^2)/den;
 a = [a0 a1 a2]; b = [b0 b1 b2];
-
-%[H, fgrid] = freqz(b,a,N,fs); %filter coeffs are correct
-%plot(fgrid, 20*log(abs(H))); grid on; axis([20, 2000, -20, 0]);
-
-% hand made plot needs to be fixed, 
-% need to have better understanding of the returning frequency vector 
-% and the calculations needed in order to calculate the (almost) "continuous" 
-% f-vector
-
-
-% [H, wo] = freqz(b,a,N);
-% fgrid = 2*atan(wo*fs/(pi)); 
-% H_ = 20*log(abs(H));
-% plot(fgrid,H_); grid on; axis([20 2000 -10 0]);
-
 end
